d4d24bebcf4324932cb2cd6cb7de592e9373bbee
ABSphreak <absphreak@outlook.com>
2020-01-21 21:49:40 +0530

    Updates

---
+++ precache-manifest.b7b469015c98ba9b703a37e7d949662f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51014d11444a447854fde051a93c0e2b",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "f8f9279dde18c01a6253",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "633b3d1018f58e717dfc",
+    "url": "/todoapp/static/js/2.d46d8f21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoapp/static/js/2.d46d8f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8f9279dde18c01a6253",
+    "url": "/todoapp/static/js/main.ab0aa679.chunk.js"
+  },
+  {
+    "revision": "63ed8d88c0187d779e28",
+    "url": "/todoapp/static/js/runtime-main.47b19ebb.js"
+  }
+]);
